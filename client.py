17aa99d1bc0447175aceaad3960da2dfc28004d5
DMComputingNE <99381622+DMComputingNE@users.noreply.github.com>
2022-03-24 02:56:43 +0000

    files

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+import hashlib
+##initial setup
+s = socket.socket()#create socket 
+#unlike the server .py, these server info are stored in variables to allow further expansion of the software for inputting an IP address instead of hard coding
+port = 55555
+ip='127.0.0.1'
+s.connect((ip, port))
+##user input collection
+name = input("What is your username: ")
+pwd= input("What is your password: ")
+##encode and send
+bytpwd=pwd.encode() #encode pwd data into byte
+data = name + ',' + hashlib.sha224(bytpwd).hexdigest() #store name and store password hash
+data = data.encode() #encode new string into byte form, and store it to data variable
+s.sendall(data)#send encoded data
+##overrite stored values to ensure they do not remain in memory
+data=""
+pwd=""
+bytpwd=""
+#
+
+print (s.recv(1024).decode())#print recieved data
+s.close()
---
+++ server.py
@@ -0,0 +1,39 @@
+from encodings import utf_8
+import random
+import socket
+from xml.etree.ElementTree import tostring
+#host info constants
+HOST = "127.0.0.1"
+PORT = 55555  
+
+#validation code, this could be expanded to search an entire database rather than use hardcoded values
+def validate(datatoval):
+    if b"denis" in datatoval:# check username
+        if b"d63dc919e201d7bc4c825630d2cf25fdc93d4b2f0d46706d29038d01" not in datatoval: #look for hash value of password in recieved string
+            return False #output true or false
+        else: 
+            return True
+
+
+#main server function
+def Recieve():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+        s.bind((HOST, PORT)) 
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            while True: # this loop continues recieving data, until there is no more data to recieve
+                data = conn.recv(1024)
+                if not data:
+                    break# stop loop when out of data
+                if validate(data): # call validate function, and if data is valid, generate and send OTP
+                    random_code =random.randint(10000,99999) # random 5 digit number
+                    conn.sendall(bytes(f"Password verified, your OTP is: {random_code}","utf_8")) #combine OTP with verification message and send in byte form
+                    
+                else:
+                    conn.sendall(b"Incorrect username/password")#inform user of wrong pass
+while(1):#main loop to handle multiple connections in a row
+    try:#simple exception handling
+        Recieve() #call main function
+    except:
+        print("Something went wrong.")#exception message on server terminal
