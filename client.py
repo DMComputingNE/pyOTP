033f2a1b14d2d61637a22b9d62e9eab060b21cb4
DMComputingNE <99381622+DMComputingNE@users.noreply.github.com>
2022-03-24 07:57:56 +0000

    aaaaaaaaa 2.0

--- client.py
+++ client.py
@@ -2,7 +2,6 @@ import socket
 import hashlib
 ##initial setup
 s = socket.socket()#create socket 
-#unlike the server .py, these server info are stored in variables to allow further expansion of the software for inputting an IP address instead of hard coding
 port = 55555
 ip=input("enter the ip: ")
 s.connect((ip, port))
